32f04bf76ee176f3b06ff5abc303211a4511ed67
Ryunosuke SATO <tricknotes.rs@gmail.com>
2011-08-05 23:48:44 +0900

    [p.100 #5.8] read from stdin

--- src/cat.c
+++ src/cat.c
@@ -5,31 +5,31 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-static void do_cat(const char *path);
+static void do_cat(int fd, const char *path);
 static void die(const char *s);
 
 int main(int argc, char *argv[]) {
-    int i;
     if (argc < 2) {
-        fprintf(stderr, "%s: file name not given\n", argv[0]);
-        exit(1);
-    }
-    for (i = 1; i < argc; i++) {
-        do_cat(argv[i]);
+        do_cat(STDIN_FILENO, "STDIN_FILENO");
+    } else {
+        int i;
+        for (i = 1; i < argc; i++) {
+            char *path = argv[i];
+            int fd = open(path, O_RDONLY);
+            do_cat(fd, path);
+        }
     }
     exit(0);
 }
 
 #define BUFFER_SIZE 2040
 
-static void do_cat(const char *path) {
-    int fd;
+static void do_cat(int fd, const char *path) {
     unsigned char buf[BUFFER_SIZE];
-    int n;
 
-    fd = open(path, O_RDONLY);
     if (fd < 0) { die(path); }
     while (1) {
+        int n;
         n = read(fd, buf, sizeof buf);
         if (n < 0) { die(path); }
         if (n == 0) { break; }
