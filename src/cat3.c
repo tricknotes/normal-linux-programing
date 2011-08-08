0c5f56eb8cd97186dfa58c15bbfe97b9e171f9a0
Ryunosuke SATO <tricknotes.rs@gmail.com>
2011-08-09 00:39:09 +0900

    [p.133 #6.11] add cat3.c

---
+++ src/cat3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void putsStdout(FILE *f, const char *path);
+
+int main(int argc, char *argv[]) {
+    int i;
+    FILE *f;
+
+    if (argc < 2) {
+        f = fdopen(STDIN_FILENO, "r");
+        putsStdout(f, argv[i]);
+    }
+    for (i = 1; i < argc; i++) {
+        f = fopen(argv[i], "r");
+        putsStdout(f, argv[i]);
+    }
+    exit(0);
+}
+
+void putsStdout(FILE *f, const char *path) {
+    int c;
+    if (!f) {
+      perror(path);
+      exit(1);
+    }
+    while((c = fgetc(f)) != EOF) {
+        if ('\t' == c) {
+            printf("\\t");
+        } else {
+            if ('\n' == c) { putchar('$'); }
+            if (putchar(c) < 0) { exit(1); }
+        }
+    }
+}
