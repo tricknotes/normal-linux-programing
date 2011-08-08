19199252c23a0c1921c2ee1355881da7787016b5
Ryunosuke SATO <tricknotes.rs@gmail.com>
2011-08-09 00:07:33 +0900

    support input from stdin

--- src/cat2.c
+++ src/cat2.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+void putsStdout(FILE *f, const char *path);
 
 int main(int argc, char *argv[]) {
     int i;
-    for (i = 1; i < argc; i++) {
-        FILE *f;
-        int c;
+    FILE *f;
 
+    if (argc < 2) {
+        f = fdopen(STDIN_FILENO, "r");
+        putsStdout(f, argv[i]);
+    }
+    for (i = 1; i < argc; i++) {
         f = fopen(argv[i], "r");
-        if (!f) {
-          perror(argv[i]);
-          exit(1);
-        }
-        while((c =fgetc(f)) != EOF) {
-            if (putchar(c) < 0) { exit(1); }
-        }
+        putsStdout(f, argv[i]);
     }
     exit(0);
+}
+
+void putsStdout(FILE *f, const char *path) {
+    int c;
+    if (!f) {
+      perror(path);
+      exit(1);
+    }
+    while((c =fgetc(f)) != EOF) {
+        if (putchar(c) < 0) { exit(1); }
+    }
 }
