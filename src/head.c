30607fed8fb4e5e844d290309398e5061c193684
Ryunosuke SATO <tricknotes.rs@gmail.com>
2011-08-16 15:48:16 +0900

    [p141 #7.1] read from file

--- src/head.c
+++ src/head.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static void do_head(FILE *f, long nlines);
+static long do_head(FILE *f, long nlines);
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s n\n", argv[0]);
+    long nlines;
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s n [file file ...]\n", argv[0]);
         exit(1);
     }
-    do_head(stdin, atol(argv[1]));
+    nlines = atol(argv[1]);
+    if (argc == 2) {
+        do_head(stdin, nlines);
+    } else {
+        int i;
+
+        for (i = 2; i < argc; i++) {
+            FILE *f;
+
+            f = fopen(argv[i], "r");
+            if (!f) {
+                perror(argv[i]);
+                exit(1);
+            }
+            nlines = do_head(f, nlines);
+            fclose(f);
+        }
+    }
     exit(0);
 }
 
-static void do_head(FILE *f, long nlines) {
+static long do_head(FILE *f, long nlines) {
     int c;
 
     while (nlines > 0 && (c = getc(f)) != EOF) {
         if (putchar(c) < 0) { exit(1); }
         if (c == '\n') { nlines--; }
     }
+    return nlines;
 }
