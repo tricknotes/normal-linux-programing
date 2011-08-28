2b9174ca2db09d9aaac335fa96396d675acc7075
Ryunosuke SATO <tricknotes.rs@gmail.com>
2011-08-29 00:01:16 +0900

    [p.180 #8.3] support option -i ant -v

--- src/grep.c
+++ src/grep.c
@@ -4,9 +4,33 @@
 #include <sys/types.h>
 #include <regex.h>
 
-static void do_grep(regex_t *pat, FILE *f);
+#define _GNU_SOURCE
+#include <getopt.h>
+
+static void do_grep(regex_t *pat, FILE *f, int invert_match);
+
+static struct option longopts[] = {
+  {"ignore-case", no_argument, NULL, 'i'},
+  {"invert-match", no_argument, NULL, 'v'},
+  {0, 0, 0, 0}
+};
 
 int main(int argc, char *argv[]) {
+  int opt;
+  int ignore_case = 0;
+  int invert_match = 0;
+
+  while ((opt = getopt_long(argc, argv, "iv", longopts, NULL)) != -1) {
+    switch (opt) {
+      case 'i':
+        ignore_case = REG_ICASE;
+        break;
+      case 'v':
+        invert_match = REG_NOMATCH;
+        break;
+    }
+  }
+
   regex_t pat;
   int err;
   int i;
@@ -15,7 +39,8 @@ int main(int argc, char *argv[]) {
     fputs("no pattern\n", stderr);
     exit(1);
   }
-  err = regcomp(&pat, argv[1], REG_EXTENDED | REG_NOSUB | REG_NEWLINE);
+  int regex_flg = REG_EXTENDED | REG_NOSUB | REG_NEWLINE | ignore_case;
+  err = regcomp(&pat, argv[optind], regex_flg);
   if (err != 0) {
     char buf[1024];
 
@@ -23,10 +48,10 @@ int main(int argc, char *argv[]) {
     puts(buf);
     exit(1);
   }
-  if (argc == 2) {
-    do_grep(&pat, stdin);
+  if (optind == argc) {
+    do_grep(&pat, stdin, invert_match );
   } else {
-    for (i = 2; i < argc; i++) {
+    for (i = optind + 1; i < argc; i++) {
       FILE *f;
 
       f = fopen(argv[i], "r");
@@ -34,7 +59,7 @@ int main(int argc, char *argv[]) {
         perror(argv[i]);
         exit(1);
       }
-      do_grep(&pat, f);
+      do_grep(&pat, f, invert_match );
       fclose(f);
     }
   }
@@ -42,11 +67,11 @@ int main(int argc, char *argv[]) {
   exit(0);
 }
 
-static void do_grep(regex_t *pat, FILE *src) {
+static void do_grep(regex_t *pat, FILE *src, int invert_match) {
   char buf[4096];
 
   while (fgets(buf, sizeof buf, src)) {
-    if (regexec(pat, buf, 0, NULL, 0) == 0) {
+    if (regexec(pat, buf, 0, NULL, 0) == invert_match) {
       fputs(buf, stdout);
     }
   }
