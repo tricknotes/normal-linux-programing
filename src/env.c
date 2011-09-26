0355b715e34f9a73c0143876e1270f915c32251b
Ryunosuke SATO <tricknotes.rs@gmail.com>
2011-09-27 03:49:44 +0900

    [p.300 #14.3] add env.c

---
+++ src/env.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char **environ;
+
+int main(int argc,  char *argv[]) {
+  char **p;
+
+  for (p = environ; *p; p++) {
+    printf("%s\n", *p);
+  }
+  exit(0);
+}
