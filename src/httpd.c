b7b00b1f491f3c6de5359bc9b74e558d457e83e2
Ryunosuke SATO <tricknotes.rs@gmail.com>
2011-10-02 23:26:22 +0900

    [p.388 #17.1] implement main() with empty

--- src/httpd.c
+++ src/httpd.c
@@ -8,6 +8,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <time.h>
+#include <getopt.h>
 
 static void log_exit(char *fmt, ...);
 static void* xmalloc(size_t size);
@@ -48,13 +49,79 @@ struct FileInfo {
   int ok;
 };
 
+#define USAGE "Usage: %s [--port=n] [--chroot --user=u --group=g] [--debug] <docroot>\n"
+#define LOG_PID 1101
+#define LOG_NDELAY 1
+#define LOG_DAEMON "./log_file"
+
+static void setup_environment(char *docroot, char *user, char *group);
+static void openlog(char *server_name, int pid, char *log);
+static int listen_socket(char *port);
+static void server_main(int server, char *docroot);
+static void become_daemon(void);
+
+static int debug_mode = 0;
+
+static struct option longopts[] = {
+  {"debug",  no_argument,       &debug_mode, 1},
+  {"chroot", no_argument,       NULL, 'c'},
+  {"user",   required_argument, NULL, 'u'},
+  {"group",  required_argument, NULL, 'g'},
+  {"port",   required_argument, NULL, 'p'},
+  {"help",   no_argument,       NULL, 'h'},
+  {0, 0, 0, 0}
+};
+
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s <docroot>\n", argv[0]);
+  int server;
+  char *port = NULL;
+  char *docroot;
+  int do_chroot = 0;
+  char *user = NULL;
+  char *group = NULL;
+  int opt;
+
+  while ((opt = getopt_long(argc, argv, "", longopts, NULL)) != -1) {
+    switch (opt) {
+      case 0:
+        break;
+      case 'c':
+        do_chroot = 1;
+        break;
+      case 'u':
+        user = optarg;
+        break;
+      case 'g':
+        group = optarg;
+        break;
+      case 'p':
+        port = optarg;
+        break;
+      case 'h':
+        fprintf(stdout, USAGE, argv[0]);
+        exit(0);
+      case '?':
+        fprintf(stderr, USAGE, argv[0]);
+        exit(1);
+    }
+  }
+  if (optind != argc - 1) {
+    fprintf(stderr, USAGE, argv[0]);
     exit(1);
   }
+  docroot = argv[optind];
+
+  if (do_chroot) {
+    setup_environment(docroot, user, group);
+    docroot = "";
+  }
   install_signal_handlers();
-  service(stdin, stdout, argv[1]);
+  server = listen_socket(port);
+  if (!debug_mode) {
+    openlog(SERVER_NAME, LOG_PID|LOG_NDELAY, LOG_DAEMON);
+    become_daemon();
+  }
+  server_main(server, docroot);
   exit(0);
 }
 
@@ -357,4 +424,24 @@ static void respond_to(struct HTTPRequest *req, FILE *out, char *docroot) {
   } else {
     not_implemented(req, out);
   }
+}
+
+static void setup_environment(char *docroot, char *user, char *group) {
+  // TODO implement this
+}
+
+static void openlog(char *server_name, int pid, char *log) {
+  // TODO implement this
+}
+
+static int listen_socket(char *port) {
+  // TODO implement this
+}
+
+static void server_main(int server, char *docroot) {
+  // TODO implement this
+}
+
+static void become_daemon(void) {
+  // TODO implement this
 }
